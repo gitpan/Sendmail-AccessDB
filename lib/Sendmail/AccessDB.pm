276295605155d9faf1f75edead7e49e81c590583
Derek J. Balling <dredd@megacity.org>
2001-08-20 01:17:03 +0000

    Import of DREDD/Sendmail-AccessDB-0.04 from CPAN.
    gitpan-cpan-distribution: Sendmail-AccessDB
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         DREDD/Sendmail-AccessDB-0.04.tar.gz
    gitpan-cpan-author:       DREDD
    gitpan-cpan-maturity:     released

--- lib/Sendmail/AccessDB.pm
+++ lib/Sendmail/AccessDB.pm
@@ -2,16 +2,18 @@
 package Sendmail::AccessDB;
 use DB_File;
 use strict;
+use Carp;
 
 BEGIN {
 	use Exporter ();
-	use vars qw ($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $regex_lock);
-	$VERSION     = 0.03;
+	use vars qw ($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $regex_lock
+		     $DB_FILE);
+	$VERSION     = 0.04;
 	@ISA         = qw (Exporter);
-	#Give a hoot don't pollute, do not export more than needed by default
 	@EXPORT      = qw ();
-	@EXPORT_OK   = qw (spam_friend whitelisted);
+	@EXPORT_OK   = qw (spam_friend whitelisted lookup);
 	%EXPORT_TAGS = ();
+	$DB_FILE = '/etc/mail/access.db';
 }
 
 =head1 NAME
@@ -43,8 +45,10 @@ second argument "Category", which could be used if you wanted to enable
 specific checks, for example, if you wanted to customize down to a per-check
 basis, you might use:
 
- $rbl_friend_or_hater = spam_friend('user@example.com','maps_rbl'); 
- $dul_friend_or_hater = spam_friend('user@example.com','maps_dul'); 
+ $rbl_friend_or_hater = spam_friend('user@example.com',
+                                    'qualifier' => 'maps_rbl'); 
+ $dul_friend_or_hater = spam_friend('user@example.com',
+                                    'qualifier' => 'maps_dul'); 
 
 Caution should be taken when defining your own categories, as they may
 inadvertantly conflict with Sendmail-defined categories.
@@ -54,7 +58,44 @@ inadvertantly conflict with Sendmail-defined categories.
  $whitelisted_host = whitelisted('foo.example.com');
  $whitelisted_addr = whitelisted('192.168.1.123');
 
-Would check for appropriate whitelisting entries in access.db.
+Would check for appropriate whitelisting entries in access.db. Some lookups
+might be ambiguous, for example:
+
+ $whitelisted = whitelisted('foobar');
+
+where it is hard to know if that is supposed to be a hostname, or a sender.
+whitelisted() accepts the 'type' argument, such as:
+
+ $whitelisted = whitelisted('foobar','type'=>'hostname');
+ $whitelisted = whitelisted('postmaster','type'=>'mail');
+
+It's also possible to feed the qualifier argument, if necessary, for example,
+to do:
+ 
+ $whitelisted = whitelisted('host.example.com','type'=>'hostname',
+                            'qualifier' => 'Connect');
+
+which would check to see if this host has an OK flag set for the Connect
+qualifier.
+
+There is also the generic "lookup", which, at its simplest, takes a single
+argument:
+
+ $rc = lookup('host.example.com');
+
+will do a lookup on host.example.com. But if you wanted to pay attention to
+parent-domains, you might do:
+
+ $rc = lookup('host.example.com', 'type'=>'hostname');
+
+but if you wanted to find out if 'host.example.com', or any of its parent 
+domains ('example.com' and 'com'), had a value in the "MyQual" qualifier, you
+might do:
+
+ $rc = lookup('host.example.com','type'=>'hostname','qualifier'=>'MyQual');
+
+which would look up, in order 'MyQual:host.example.com', 'MyQual:example.com',
+and 'MyQual:com', returning the first (most specific) one found.
 
 =head1 BUGS
 
@@ -94,11 +135,12 @@ These are how you should interact with this module.
 
 =head2 spam_friend
 
- Usage     : $friend_or_hater = spam_friend($recipient,[$category])
+ Usage     : $friend_or_hater = spam_friend($recipient,
+                                            ['qualifier' => $category])
  Purpose   : Consults the /etc/mail/access.db to check for spamfriendliness
- Returns   : 'friend','hater', or undef (which would mean default 
+ Returns   : 'FRIEND','HATER', or undef (which would mean default 
              behavior for that site)
- Argument  : The recipient e-mail address and an optional category if
+ Argument  : The recipient e-mail address and an optional qualifier if
              the default of 'Spam' is not desired. 
  Throws    : 
  Comments  : 
@@ -109,38 +151,8 @@ These are how you should interact with this module.
 sub spam_friend
 {
     my $address = shift;
-    my $category = shift || 'Spam';
-    my %access;
-    tie %access,'DB_File', "/etc/mail/access.db";
-    
-    my @to_check = ("Spam:$address");
-    {
-        lock $regex_lock;
-        my ($left,$right) = $address =~ /^(.*\@)(.*)/;
-        push @to_check, ("$category:$left") if defined $left;
-        push @to_check, ("$category:$right") if defined $right;
-    }
-    
-    my $friend = 0; my $hater = 0;
-    foreach my $dirty_check_me (@to_check)
-    {
-        my $check_me = lc $dirty_check_me;
-        if (defined $access{$check_me})
-        {
-            if ( uc($access{$check_me}) eq 'FRIEND' )
-            {
-                $friend = 1;
-            }
-            if ( uc($access{$check_me}) eq 'HATER' )
-            {
-                $hater = 1;
-            }
-        }
-    }
-
-    if ($friend) { return 'FRIEND'; }
-    if ($hater) { return 'HATER'; }
-    return undef;
+    my $qual = shift || 'Spam';
+    return lookup($address,'qualifier'=>$qual,'type'=>'mail');
 }
 
 =head2 whitelisted
@@ -151,8 +163,12 @@ sub spam_friend
  Returns   : 0/1, true or false as to whether the argument is whitelisted
  Argument  : Either an email-address (e.g., foo@example.com), an IP address
              (e.g., 10.200.1.230), or a hostname (e.g., mailhost.example.com)
- Throws    : Nothing at present.
- Comments  : This is pretty new, use at your own risk :)
+             as well as 'type' and 'qualifer' arguments (see lookup for greater
+             detail)
+ Throws    : 
+ Comments  : The code makes a pretty good attempt to figure out what type
+             of argument $value is, but it can be overriden using the 'type'
+             qualifier.
 See Also   : 
 
 =cut
@@ -160,64 +176,148 @@ See Also   :
 sub whitelisted
 {
     my $address = shift;
-    my %access;
-    tie %access, 'DB_File', "/etc/mail/access.db";
+    my %args = @_;
     
-    my @to_check = ($address);
-    my $RHS;
+    if (! defined $args{'type'})
     {
-        lock $regex_lock;
-        if ($address =~ /^(.*)\@(.*)/)
+        if ($address =~ /\@/)
         {
-            my ($left,$right) = $address =~ /^(.*\@)(.*)/;
-            push @to_check, ("$left") if defined $left;
-            push @to_check, ("$right") if defined $right;
-	    $RHS = $right if defined $right;
+	    $args{'type'} = 'mail';
         }
         elsif ($address =~ /^(?:\d+\.){3}\d+/)
         {
-            my $shorter = $address;
-            $address =~ s/\.\d+$//;
-            push @to_check, ($shorter);
-            $address =~ s/\.\d+$//;
-            push @to_check, ($shorter);
-            $address =~ s/\.\d+$//;
-            push @to_check, ($shorter);
+	    $args{'type'} = 'ip';
         }       
-        elsif ($address =~ /^[\w\-\.]+$/)
+        elsif ($address =~ /^[A-Za-z0-9\-\.]+$/)
         {
-            while (my ($shorter) = $address =~ /^[\w\-]+\.(.*)$/)
-            {
-                push @to_check, $shorter;
-                $address = $shorter;
-            }
+	    $args{'type'} = 'hostname';
         }
-	if (defined $RHS)
-	{
-            while (my ($shorter) = $RHS =~ /^[\w\-]+\.(.*)$/)
-            {
-                push @to_check, $shorter;
-                $RHS = $shorter;
-            }
-	}	    
     }
-    my $ok = 0;
-    foreach my $dirty_check_me (@to_check)
-    {
-        my $check_me = lc $dirty_check_me;
-        if (defined $access{$check_me})
-        {
-            if ( uc($access{$check_me}) eq 'OK' )
-            {
-                $ok = 1;
-            }
-        }
-
-    }
-
-    return $ok;
+    my $lookup = lookup($address,%args);
+    return ( (defined $lookup) and ($lookup eq 'OK') ) ? 1 : 0;
 }
 
+=head2 lookup
+
+ Usage     : lookup ($lookup_key, [ 'type'=>{'mail','ip','hostname'} ,
+                                    'qualifier'=>'qualifier' ])
+ Purpose   : Do a generic lookup on a $lookup_key in the access.db and
+             return the value found (or undef if not)
+ Returns   : value in access.db or undef if not found
+ Argument  : $lookup_key - mandatory. 'type'=>mail/ip/hostname will cause
+             lookups against all necessary lookups according to sendmail logic
+             (for things like hostname lookups where subdomains inherit 
+             attributes of parent domains, etc.), 'qualifier'=>$q, where $q 
+             will be preprended to the beginning of all lookups, (e.g., $q =
+             'Spam', lookup would be against 'Spam:lookup_value')
+ Throws    : 
+ Comments  : If not using 'type', the 'qualifier' field can be mimicked by 
+             simply looking for 'Qualifier:lookup'.
+ See Also  : 
+
+=cut
+
+
+
+sub lookup
+{
+    my ($address,%args) = @_;
+    my @check_list;
+    if (defined $args{'type'})
+    {
+	if ($args{'type'} eq 'mail')
+	{
+	    @check_list = _expand_email($address);
+	}
+	elsif ($args{'type'} eq 'hostname')
+	{
+	    @check_list = _expand_hostname($address);
+	}
+	elsif ($args{'type'} eq 'ip')
+	{
+	    @check_list = _expand_ip($address);
+	}
+    }
+    else
+    {
+	@check_list = ($address);
+    }
+
+    my %access;  my $rc;
+
+    tie %access, 'DB_File', $DB_FILE , O_RDONLY or croak "Couldn't open $DB_FILE : $!";
+
+    foreach my $key (@check_list)
+    {	
+	my $lookup = $key;
+
+	if (defined $args{'qualifier'})
+	{
+	    $lookup = "$args{'qualifier'}:$lookup";
+	}
+	$lookup = lc $lookup;
+#	print "looking up '$lookup'\n";
+	if ($access{$lookup})
+	{
+	    return $access{$lookup};
+	}
+    }
+
+    untie %access;
+    return $rc;
+}
+    
+
+
+sub _expand_ip
+{
+    my $address = shift;
+    my @expanded = ();
+    if ($address =~ /^(?:\d+\.){3}\d+/)
+    {
+	push @expanded, $address;
+	my $shorter = $address;
+	$shorter =~ s/\.\d+$//;
+	push @expanded, ($shorter);
+	$shorter =~ s/\.\d+$//;
+	push @expanded, ($shorter);
+	$shorter =~ s/\.\d+$//;
+	push @expanded, ($shorter);
+    }
+    return @expanded;
+}
+
+sub _expand_hostname : locked
+{
+    my $hostname = shift;
+    my @expanded = ($hostname);
+    while (my ($shorter) = $hostname =~ /^[\w\-]+\.(.*)$/)
+    {
+	push @expanded, ($shorter) if $shorter;
+	$hostname = $shorter;
+    }
+    return @expanded;
+} 
+
+sub _expand_email : locked
+{
+    my $address = shift;
+    my @to_check = ($address);
+    if ($address !~ /\@/)
+    {
+	push @to_check, ("$address\@");
+    }
+    elsif ($address =~ /^.*\@[A-Za-z0-9.\-]*/)
+    {
+	my ($left,$right) = $address =~ /^(.*\@)([A-Za-z0-9.\-]*)$/;
+	push @to_check, ($left) if (defined $left) and ($left) and ($left ne $address);
+	if ( (defined $right) and ($right) )
+	{
+	    push @to_check, ( _expand_hostname($right) );
+	}
+    }
+    return @to_check;
+}
 
 
 =head1 PRIVATE METHODS
@@ -227,7 +327,19 @@ These methods and functions are considered private and are intended for
 internal use by this module. They are B<not> considered part of the public
 interface and are described here for documentation purposes only.
 
-(none)
+=head2 _expand_ip
+=head2 _expand_hostname
+=head2 _expand_address
+
+ Usage     : @expanded = _expand_ip($ip); # For example
+ Returns   : Given an ip, hostname, or e-mail address, it will expand
+             that into the "appropriate lookups" which sendmail would use
+             (e.g., given '192.168.1.2', _expand_ip would return
+             192.168.1.2, 192.168.1, 192.168, and 192)
+ Argument  : The IP Address, hostname, or e-mail address to expand
+ Throws    : 
+ Comments  : 
+ See Also  : 
 
 =cut
 
---
+++ t/03_functions.t
@@ -0,0 +1,41 @@
+# t/03_functions.t; test the basic functions
+
+$|++; 
+print "1..5
+";
+my($test) = 1;
+
+# 1 load
+use Sendmail::AccessDB;
+my $foo;
+
+$Sendmail::AccessDB::DB_FILE="./test.db";
+$Sendmail::AccessDB::DB_FILE="./t/test.db";
+
+
+
+my $friend = Sendmail::AccessDB::spam_friend('foo@bar.com');
+if ( (defined $friend) and ($friend eq 'FRIEND')) 
+  { print "ok $test\n";}else{ print "not ok $test\n"; }
+$test++;
+
+my $whitelisted = Sendmail::AccessDB::whitelisted('foo.test.example.com','type'=>'hostname');
+if ( (defined $whitelisted) and ($whitelisted))
+  { print "ok $test\n" }else{ print "not ok $test\n"; }
+$test++;
+
+my $should_fail = Sendmail::AccessDB::whitelisted('bar.example.com');
+if ($should_fail) { print "not ok $test\n";} else { print "ok $test\n"; }
+$test++;
+
+my $lookup = Sendmail::AccessDB::lookup('foo.bar.tld','qualifier'=>'Qual');
+if ( (defined $lookup) and ($lookup eq 'OK'))
+   { print "ok $test\n" } else { print "not ok $test\n"; };
+$test++;
+
+my $wltwo = Sendmail::AccessDB::whitelisted('user@foo.bar.tld','qualifier'=>'Qual','type'=>'mail');
+if ($wltwo) { print "ok $test\n" } else { print "not ok $test\n"; };
+$test++;
+
+# end of t/03_functions.t
+
